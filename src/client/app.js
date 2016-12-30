9b84e3e22ce69fd7f09a386884b314e1225ce97d
Antonio Bianchetti <antonio.bianchetti@netcentric.biz>
2016-12-30 16:46:26 +0100

    Added React

--- src/client/app.js
+++
@@ -1,6 +0,0 @@
-import 'babel-polyfill'; // check if it's needed
-import Dog from '../shared/dog';
-
-const browserToby = new Dog('Browser Toby');
-
-document.querySelector('.app').innerText = browserToby.bark();
