9b5d9856a271a6e7151672aacc86ee9f816811c1
Antonio Bianchetti <antonio.bianchetti@netcentric.biz>
2016-12-30 19:57:20 +0100

    Added Redux

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,21 +1,21 @@
-import 'babel-polyfill';
-
-import React, { PropTypes } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Dog from '../shared/dog';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import dogReducer from './reducers/dog-reducer';
+import BarkMessage from './containers/bark-message';
+import BarkButton from './containers/bark-button';
 
-const dogBark = new Dog('Browser Toby').bark();
-
-const App = props => (
-  <div>
-    The dog says: {props.message}
-  </div>
-);
-
-App.propTypes = {
-  message: PropTypes.string.isRequired
-};
+const store = createStore(combineReducers({
+  dog: dogReducer
+}));
 
 ReactDOM.render(
-  <App message={dogBark} />,
-  document.querySelector('.app'));
+  <Provider store={store}>
+    <div>
+      <BarkMessage />
+      <BarkButton />
+    </div>
+  </Provider>
+  , document.querySelector('.app')
+);
