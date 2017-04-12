f7213372d64e8b4186e926a0856c5cf5489482b8
Seuck <abianchetti@yahoo.com>
2017-04-12 17:56:30 +0200

    Initial navigation development

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,17 @@
 function init() {
   const logo = document.querySelector('.header__logo');
+  const navigation = document.querySelector('.header__navigation');
   window.addEventListener('scroll', () => {
     const distanceY = window.pageYOffset || document.documentElement.scrollTop;
     if (distanceY >= 0) {
       const scale = Math.max(0.3, 1 - ((distanceY / 100.0) / 3.5));
       logo.style.transform = `scale(${scale})`;
+
+      if (distanceY > 250) {
+        navigation.className = 'header__navigation visible';
+      } else {
+        navigation.className = 'header__navigation';
+      }
     }
   });
 }
