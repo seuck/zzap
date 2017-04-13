4fd48d85d9ac3fea8505900b6557c52f867e128a
Seuck <abianchetti@yahoo.com>
2017-04-13 12:54:43 +0200

    Improved cover for mobile

--- src/client/app.js
+++ src/client/app.js
@@ -1,18 +1,31 @@
-function init() {
-  const logo = document.querySelector('.header__logo');
-  const navigation = document.querySelector('.header__navigation');
-  window.addEventListener('scroll', () => {
-    const distanceY = window.pageYOffset || document.documentElement.scrollTop;
-    if (distanceY >= 0) {
-      const scale = Math.max(0.3, 1 - ((distanceY / 100.0) / 3.5));
-      logo.style.transform = `scale(${scale})`;
+(() => {
+  function initScroll(window, document) {
+    const logo = document.querySelector('.header__logo');
+    const navigation = document.querySelector('.header__navigation');
 
-      if (distanceY > 250) {
-        navigation.className = 'header__navigation visible';
-      } else {
-        navigation.className = 'header__navigation';
+    window.addEventListener('scroll', () => {
+      const minScale = 0.3;
+      const windowWidthMaxLogoSize = 900;
+      const speedFactor = 250;
+      const distanceY = window.pageYOffset || document.documentElement.scrollTop;
+      const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+      const speed = Math.min(windowWidth, windowWidthMaxLogoSize) / speedFactor;
+
+      if (distanceY >= 0) {
+        const scale = Math.max(minScale, 1 - ((distanceY / 100.0) / speed));
+        logo.style.transform = `scale(${scale})`;
+
+        if (distanceY > 250) {
+          navigation.className = 'header__navigation visible';
+        } else {
+          navigation.className = 'header__navigation';
+        }
       }
-    }
-  });
-}
-window.onload = init();
+    });
+  }
+
+  window.onload = () => {
+    initScroll(window, document);
+  };
+})();
+
