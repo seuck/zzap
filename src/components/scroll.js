fdebb34776ce14d93599f74f933937e7d0dc898a
Seuck <abianchetti@yahoo.com>
2017-05-01 14:00:50 +0200

    Attempt to improve scrolling behaviour

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -3,32 +3,42 @@ import $ from 'jquery';
 (() => {
   function initScroll(window, document) {
     const logo = document.querySelector('.header__logo');
-    const navigation = document.querySelector('.header__navigation');
+    const headerClass = 'header';
+    const headerFlavorCompactClass = `${headerClass}--compact`;
+    const header = document.querySelector(`.${headerClass}`);
     const minScale = 0.3;
-    const windowWidthMaxLogoSize = 900;
-    const speedFactor = 250;
+    const windowWidthMaxSize = 900;
+    const verticalThresholdMax = 250;
     let lastYOffset = -1;
+    let lastWindowWidth = -1;
 
     function refreshHeader() {
-      if (lastYOffset === window.pageYOffset) {
+      const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+      if (lastYOffset === window.pageYOffset && lastWindowWidth === windowWidth) {
         requestAnimationFrame(refreshHeader);
       } else {
         lastYOffset = window.pageYOffset;
+        lastWindowWidth = windowWidth;
 
         const distanceY = window.pageYOffset || document.documentElement.scrollTop;
-        const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        const speed = Math.min(windowWidth, windowWidthMaxLogoSize) / speedFactor;
-
         if (distanceY >= 0) {
-          const scale = Math.max(minScale, 1 - ((distanceY / 100.0) / speed));
-          logo.style.transform = `scale(${scale})`;
-
-          if (distanceY > 250) {
-            navigation.className = 'header__navigation visible';
+          const limitedWindowWidth = Math.min(windowWidth, windowWidthMaxSize);
+          const actualThreshold = (verticalThresholdMax / windowWidthMaxSize) * limitedWindowWidth;
+          if (distanceY > actualThreshold) {
+            header.className = `${headerClass} ${headerFlavorCompactClass}`;
+            logo.style.transform = `scale(${minScale})`;
           } else {
-            navigation.className = 'header__navigation';
+            const speed = limitedWindowWidth / verticalThresholdMax;
+            const scale = Math.max(minScale, 1 - ((distanceY / 100.0) / speed));
+            logo.style.transform = `scale(${scale})`;
+
+            header.className = headerClass;
           }
+        } else {
+          logo.style.transform = 'scale(1)';
         }
+
         requestAnimationFrame(refreshHeader);
       }
     }
